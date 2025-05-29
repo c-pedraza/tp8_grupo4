511bc9b0fd3085a6991c2e6aad71d75a38df041c
Carlos Pedraza Antona <pedrazaantonacarlos@gmail.com>
2025-05-29 15:12:45 +0200

    Initial commit

---
+++ src/main/java/com/indra/tp8_grupo4/Tp8Grupo4Application.java
@@ -0,0 +1,13 @@
+package com.indra.tp8_grupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp8Grupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp8Grupo4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/indra/tp8_grupo4/Tp8Grupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indra.tp8_grupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp8Grupo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
