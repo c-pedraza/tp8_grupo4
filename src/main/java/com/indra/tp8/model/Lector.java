b17740bdc243780cfe1f332e6ff7f71e38bf6fe4
Carlos Pedraza Antona <pedrazaantonacarlos@gmail.com>
2025-05-30 11:43:51 +0200

    Update Lector Lombok

--- src/main/java/com/indra/tp8/model/Lector.java
+++ src/main/java/com/indra/tp8/model/Lector.java
@@ -11,7 +11,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @Entity
 public class Lector {
 
